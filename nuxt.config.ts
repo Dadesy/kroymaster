33c5fe137708cc3e444e0c08afa41edf134b598d
konstantin <k.podkovyrov@efko.ru>
2026-03-16 02:35:24 +0300

    feat: add КройМастер branding, SVG favicon and updated SEO metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=5',
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
